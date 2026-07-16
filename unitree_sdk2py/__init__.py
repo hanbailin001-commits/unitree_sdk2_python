d1746d3830b1d14cd13af5e915a54a668af43dba
hanbailin001-commits <hanbailin001@gmail.com>
2026-07-17 03:16:41 +0700

    chore: export actions in package __init__

--- unitree_sdk2py/__init__.py
+++ unitree_sdk2py/__init__.py
@@ -1,4 +1,4 @@
-from . import idl, utils, core, rpc, comm, go2, b2, g1, h1, h2, a2, as2
+from . import idl, utils, core, rpc, comm, go2, b2, g1, h1, h2, a2, as2, actions
 
 __all__ = [
     "idl",
@@ -13,4 +13,5 @@ __all__ = [
     "h2",
     "a2",
     "as2",
+    "actions",
 ]
